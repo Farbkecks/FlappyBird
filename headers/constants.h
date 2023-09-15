b36502ff112ae2de7534eab02200353c1db92ba0
fabian <@>
2023-09-15 13:33:11 +0200

    move birt

--- headers/constants.h
+++ headers/constants.h
@@ -1,24 +1,24 @@
 #pragma once
 
 namespace constants {
-    namespace bird {
-        const sf::String path = "assets/FlappyBird.png";
-        const sf::IntRect birdUpArea = {0, 0, 255, 180};
-        const sf::IntRect birdDownArea = {255, 0, 255, 180};
-        const sf::Vector2f startPos = {200, 400};
-        const float scale = 0.3;
-
-        const float jumpVelocity = 15;
-        const float stepChangeVelocityPerUpdate = -1;
-        const float maxVelocity = 20;
-        const float minVelocity = -13;
-    }
     namespace engine {
         const unsigned int fps = 144;
         const sf::String titel = "Test";
         const sf::Vector2u resolution = {1280, 720};
         //TODO put usefully numbers
         const auto updateCycle = sf::seconds(.1f / 7.2).asSeconds();
+    }
+    namespace bird {
+        const sf::String path = "assets/FlappyBird.png";
+        const sf::IntRect birdUpArea = {0, 0, 255, 180};
+        const sf::IntRect birdDownArea = {255, 0, 255, 180};
+        const sf::Vector2f startPos = {(float)engine::resolution.x/2, 400};
+        const float scale = 0.3;
+
+        const float jumpVelocity = 15;
+        const float stepChangeVelocityPerUpdate = -1;
+        const float maxVelocity = 20;
+        const float minVelocity = -13;
     }
 
     namespace pipe {
